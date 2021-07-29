a0de3c2fdb308ca42f37ada2248b7a260b62a121
Riccardo Cipolleschi <riccardo.cipolleschi@truelayer.com>
2021-07-29 16:09:45 +0200

    Initial Commit

---
+++ Calc/Calc.h
@@ -0,0 +1,18 @@
+//
+//  Calc.h
+//  Calc
+//
+//  Created by Riccardo Cipolleschi on 29/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Calc.
+FOUNDATION_EXPORT double CalcVersionNumber;
+
+//! Project version string for Calc.
+FOUNDATION_EXPORT const unsigned char CalcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Calc/PublicHeader.h>
+
+
