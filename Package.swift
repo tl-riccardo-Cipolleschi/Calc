8679dfc86f8f6bfbd845f9c2759112dbfefacaca
Riccardo Cipolleschi <riccardo.cipolleschi@truelayer.com>
2021-07-29 16:46:11 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Calc",
+    products: [
+        .library(
+            name: "Calc", targets: ["Calc"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryFramework(
+            url: "https://github.com/tl-riccardo-Cipolleschi/Calc",
+            version: "0.0.1",
+            checksum: "d4eca46fecdb2ad200870a5cd28b6b4979f2af3debf8b0fae12594a63a142c33"
+        )
+    ]
+)
