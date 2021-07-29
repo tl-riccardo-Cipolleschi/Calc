3c078cc40941c28245559c19e7e6986618d39665
Riccardo Cipolleschi <riccardo.cipolleschi@truelayer.com>
2021-07-29 16:48:10 +0200

    fix: update package

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryFramework(
-            url: "https://github.com/tl-riccardo-Cipolleschi/Calc",
+            url: "https://github.com/tl-riccardo-Cipolleschi/Calc/releases/download/0.0.1/Calc.xcframework.zip",
             version: "0.0.1",
             checksum: "d4eca46fecdb2ad200870a5cd28b6b4979f2af3debf8b0fae12594a63a142c33"
         )
